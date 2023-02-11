46b11e5c66483c9709d9ab85114fa9cb7b14f965
Stiflerzak <hilarymwendia97@gmail.com>
2023-02-11 16:07:44 +0300

    done With assignment

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+require 'pry'
+class Dog  
+    #class body
+
+
+    #instance Method Definition
+    def bark
+        puts "Woof!"
+    end  
+
+    def sit
+        puts "The Dog is sitting"
+    end  
+end
+
+fido= Dog.new
+fido.bark
+fido.sit
+
+max= Dog.new
+max.bark
+
+
+
+
---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+    #class body
+
+    def talk 
+        puts "Hello World!"
+    end
+
+    def walk
+        puts "The Person is walking"
+    end
+
+
+end
+
+
+John = Person.new
+John.talk
+
+John.walk
